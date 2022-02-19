e7809551dcbaba5479bc3c84af03d24258b0cfd6
aog11 <antonifco@gmail.com>
2022-02-19 14:50:32 -0400

    Adding the other operations to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,28 +1,49 @@
 #Calculator script
 import sys
 
+#Initial variables
+a = None
+b = None
+
 #Taking values sent by a "lazy" Jenkins implementation
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+if len(sys.argv) > 2:
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
 
 #Addition
 def addition(a, b):
     return a + b
 
-#TODO: Substraction
+#Substraction
+def substraction(a, b):
+    return a - b
 
-#TODO: Multiplication
+#Multiplication
+def multiplication(a, b):
+    return a * b
 
-#TODO: Division
+#Division
+def division(a, b):
+    return a / b
 
 #Receive the values by input
-if a == '':
+if a is None:
     print('Please inser the first value: ', end ='')
     a = int(input())
 
-if b == '':
+if b is None:
     print('Please inser the second value: ', end ='')
     b = int(input())
 
+#Results
 result = addition(a,b)
-print ('The result is: %s' %(result))
+print ('The addition result is: %s' %(result))
+
+result = substraction(a,b)
+print ('The substraction result is: %s' %(result))
+
+result = multiplication(a,b)
+print ('The multiplication result is: %s' %(result))
+
+result = division(a,b)
+print ('The division result is: %s' %(result))
